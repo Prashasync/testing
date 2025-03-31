ce9cd4f6f47efb6dbebbc1fe53c2ef5cb74542fb
Prashasync <prasannakumar@prashasync.io>
2025-03-31 15:48:44 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // Implement event listeners if needed
     },
-    baseUrl: 'http://172.31.29.21:3000', // Change as per your app
+    baseUrl: 'http://127.0.0.1:46489', // Change as per your app
     supportFile: false, // Ensure Cypress doesn't fail due to missing support file
   },
 });
