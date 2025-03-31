080190a6e4f1feec1132ba7e787288374da12903
Prashasync <prasannakumar@prashasync.io>
2025-03-31 16:19:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-
+stage('Run Integration Tests') {
+    steps {
+        script {
+            if (fileExists('tests/integration')) {
+                sh 'npm run test:integration'
+            } else {
+                echo 'Skipping integration tests: No test files found'
+            }
+        }
+    }
+}
