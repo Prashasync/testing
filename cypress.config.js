e8dd4bb0d9193a67c10e141b24099515bfaee68a
Prashasync <prasannakumar@prashasync.io>
2025-04-01 21:06:12 +0530

    Delete cypress.config.js

--- cypress.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  e2e: {
-    baseUrl: "http://127.0.0.1:3000",
-    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",  // Ensure test files match this pattern
-    supportFile: "cypress/support/e2e.js",
-  },
-};
